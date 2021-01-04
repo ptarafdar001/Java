7bd9a77ffcf107985b77043687badfba8da5f986
prabir  Tarafdar <49672366+ptarafdar001@users.noreply.github.com>
2021-01-04 23:40:45 +0530

    1.6.2 StringClass2

---
+++ StrCls2.java
@@ -0,0 +1,23 @@
+class StrCls2
+{
+
+  public static void main(String args[])
+  {
+    String s1="Pgec";
+    String s3="Pgec";
+    String s2= new String("Pgec");
+    String s4= new String("Pgec");
+   // String comparison
+   
+   if(s2==s4)
+   {
+    System.out.println("Both strings are same");
+   }
+   else
+   {
+    System.out.println("Given strings are not equal");
+   }
+
+  }
+
+}
