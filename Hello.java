2f78588310a36b1694f8702e4f4ef3caa3c0d794
prabir  Tarafdar <49672366+ptarafdar001@users.noreply.github.com>
2021-01-04 23:19:59 +0530

    1 Hello World

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*This is my first program in java
+Author: Prabir Tarafdar
+Version: v1.0
+Project Titile: Object Oriented Programming with JAVA
+*/
+
+//import java.lang.System;
+//import java.lang.String;
+import java.lang.*;
+// java library---Package---Class--Methods
+class Hello
+{
+
+ public static void main(String args[])
+//static public void main(String args[])
+ //static public void main(String x[])
+
+{
+  System.out.print("\nHello PGEC\n");
+}
+
+}
