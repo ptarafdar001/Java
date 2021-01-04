84185cc4a33c661028e3551a701ed1c904a3d8a6
prabir  Tarafdar <49672366+ptarafdar001@users.noreply.github.com>
2021-01-04 23:41:45 +0530

    1.6.3 StringClass3

---
+++ StrCls3.java
@@ -0,0 +1,36 @@
+class StrCls3
+{
+
+  public static void main(String args[])
+  {
+  
+   /*
+    String s1="Pgec";
+    String s2="Pgec";
+
+   
+  if(s1==s2)
+   {
+    System.out.println("Both strings are same");
+   }
+   else
+   {
+    System.out.println("Given strings are not equal");
+   }
+   */
+
+     String s1 =new String("Pgec");
+    String s2 =new String("Pgec");
+   // String comparison
+   if(s1.equals(s2))
+     {
+     System.out.println("Both strings are equal"); 
+     }
+   else
+      {
+       System.out.println("Given strings are not equal"); 
+      }
+  
+  }
+
+}
