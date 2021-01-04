a76b514e4c0b35cab82178ff54a7875002b24abf
prabir  Tarafdar <49672366+ptarafdar001@users.noreply.github.com>
2021-01-04 23:24:22 +0530

    1.1 addition

---
+++ Addition.java
@@ -0,0 +1,16 @@
+//import java.lang.*;
+class Addition
+{
+
+public static void main(String args[])
+{
+  System.out.print("\n ::::Additon of two numbers::::\n");
+  int a=10, b=20;
+  int c=a+b;
+  System.out.print("Sum of a and b is::"+c+"\n"); //30 
+  System.out.println("Sum of a and b is::" +a+b); // 1020
+  System.out.println("Sum of a and b is::" +(a+b));// 30
+
+}
+
+}
